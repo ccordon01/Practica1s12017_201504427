b161c87c975b404b09681995a646dc2587457256
cech <cech@Carlos.lan>
2017-02-12 23:31:38 -0600

    Listas y Matriz Ortogonal creada!!

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author cech
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner e = new Scanner(System.in);
+        Matriz m = new Matriz(4, 4);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (m.item(2, 2) != null) {
+                    System.out.println("nodo existe");
+                } else {
+                    System.out.println("pise");
+                }
+            }
+        }
+        if (m.primerNodo == null) {
+            System.out.println("pise");
+        }
+        if (m.primerNodo.siguienteNodo == null) {
+            System.out.println("pise x2");
+        }
+        if (m.primerNodo.inferiorNodo == null) {
+            System.out.println("pise x3");
+        }
+
+        /*ListaCircular personas=new ListaCircular();
+        System.out.println("Ingrese jugador: ");
+        String repetir="";
+        do {
+            System.out.println("Ingrese nombre");
+            personas.insertarAlFinal(e.next());
+            System.out.println("Ingrese n para salir");
+            repetir=e.next();
+        } while (repetir!="n");*/
+    }
+
+}
---
+++ src/javaapplication1/ListaCircular.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author cech
+ */
+public class ListaCircular {
+    ListaSimple jugadores;
+    NodoLista primerNodo;
+    NodoLista ultimoNodo;
+
+    public ListaCircular() {
+        this.jugadores = null;
+        this.primerNodo = null;
+        this.ultimoNodo = null;
+    }
+    
+    public void insertarAlFinal(Object ei){
+    if(estaVacia())
+    {
+    primerNodo=ultimoNodo= new NodoLista(ei);
+    }
+    else{
+    ultimoNodo=ultimoNodo.siguienteNodo= new NodoLista(ei).siguienteNodo=primerNodo;
+    }
+    }
+    
+    public NodoLista nodoPosicion(int posicion){
+        NodoLista temp=primerNodo;
+        if(!estaVacia()){
+        int c=0;
+        //System.out.println(" ");
+        do {            
+            //7System.out.println("Lista: "+ c);
+            //System.out.println(actual.lista.nombre);
+            //actual.lista.imprimir();
+            temp=temp.siguienteNodo;
+            c++;
+        } while (posicion!=c);
+        }
+        return temp;
+    }
+    
+    public int getsize() {
+        
+        NodoLista actual= primerNodo;
+    int c=1;
+        //System.out.println(" ");
+        do {            
+            //7System.out.println("Lista: "+ c);
+            //System.out.println(actual.lista.nombre);
+            //actual.lista.imprimir();
+            actual=actual.siguienteNodo;
+            c++;
+        } while (actual!=ultimoNodo && actual!=null);
+        return c;
+    }
+    
+    public boolean estaVacia(){
+    return primerNodo==null;
+    }
+    
+}
